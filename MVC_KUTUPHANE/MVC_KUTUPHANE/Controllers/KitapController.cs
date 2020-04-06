658fe87a4ec5aadbbd17d859692f28c2db6bc2aa
YİĞİT KARAKOYUN <yigitkarakoyun@outlook.com>
2020-04-06 18:33:25 +0300

    Dropdown

--- MVC_KUTUPHANE/MVC_KUTUPHANE/Controllers/KitapController.cs
+++ MVC_KUTUPHANE/MVC_KUTUPHANE/Controllers/KitapController.cs
@@ -15,5 +15,33 @@ namespace MVC_KUTUPHANE.Controllers
             var temp = db.TBL_KITAP.ToList();
             return View(temp);
         }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            List<SelectListItem> temp_KategoriListesi = (from i in db.TBL_KATEGORI.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = temp_KategoriListesi;
+
+            List<SelectListItem> temp_YazarListesi = (from i in db.TBL_YAZAR.ToList()
+                                                         select new SelectListItem
+                                                         {
+                                                             Text = i.AD+" "+i.SOYAD,
+                                                             Value = i.ID.ToString()
+                                                         }).ToList();
+            ViewBag.dgr2 = temp_YazarListesi;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(TBL_KITAP p)
+        {
+            db.TBL_KITAP.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+
     }
 }
