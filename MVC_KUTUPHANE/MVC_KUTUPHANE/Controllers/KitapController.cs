e1666a7900f078c78c40316d8fcb84b90effdf00
YİĞİT KARAKOYUN <yigitkarakoyun@outlook.com>
2020-04-06 19:40:29 +0300

    cs

--- MVC_KUTUPHANE/MVC_KUTUPHANE/Controllers/KitapController.cs
+++ MVC_KUTUPHANE/MVC_KUTUPHANE/Controllers/KitapController.cs
@@ -10,10 +10,15 @@ namespace MVC_KUTUPHANE.Controllers
     {
         // GET: Kitap
         DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
-        public ActionResult Index()
+        public ActionResult Index(string p)
         {
-            var temp = db.TBL_KITAP.ToList();
-            return View(temp);
+            //var temp = db.TBL_KITAP.ToList();
+            var temp = from k in db.TBL_KITAP select k;
+            if (!string.IsNullOrEmpty(p))
+            {
+                temp = temp.Where(m=>m.AD.Contains(p));
+            }
+            return View(temp.ToList());
         }
         [HttpGet]
         public ActionResult Ekle()
@@ -29,7 +34,7 @@ namespace MVC_KUTUPHANE.Controllers
             List<SelectListItem> temp_YazarListesi = (from i in db.TBL_YAZAR.ToList()
                                                          select new SelectListItem
                                                          {
-                                                             Text = i.AD+" "+i.SOYAD,
+                                                             Text = i.AD+' '+i.SOYAD,
                                                              Value = i.ID.ToString()
                                                          }).ToList();
             ViewBag.dgr2 = temp_YazarListesi;
@@ -38,7 +43,7 @@ namespace MVC_KUTUPHANE.Controllers
         [HttpPost]
         public ActionResult Ekle(TBL_KITAP p)
         {
-            var kategori = db.TBL_KATEGORI.Where(k=>k.ID == p.ID).FirstOrDefault();
+            var kategori = db.TBL_KATEGORI.Where(k=>k.ID == p.TBL_KATEGORI.ID).FirstOrDefault();
             var yazar = db.TBL_YAZAR.Where(y=>y.ID == p.TBL_YAZAR.ID).FirstOrDefault();
             p.TBL_KATEGORI = kategori;
             p.TBL_YAZAR = yazar;
@@ -46,6 +51,49 @@ namespace MVC_KUTUPHANE.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Sil(int id)
+        {
+            var temp = db.TBL_KITAP.Find(id);
+            db.TBL_KITAP.Remove(temp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Getir(int id)
+        {
+            var p = db.TBL_KITAP.Find(id);
+            List<SelectListItem> temp_KategoriListesi = (from i in db.TBL_KATEGORI.ToList()
+                                                         select new SelectListItem
+                                                         {
+                                                             Text = i.AD,
+                                                             Value = i.ID.ToString()
+                                                         }).ToList();
+            ViewBag.dgr1 = temp_KategoriListesi;
 
+            List<SelectListItem> temp_YazarListesi = (from i in db.TBL_YAZAR.ToList()
+                                                      select new SelectListItem
+                                                      {
+                                                          Text = i.AD + ' ' + i.SOYAD,
+                                                          Value = i.ID.ToString()
+                                                      }).ToList();
+            ViewBag.dgr2 = temp_YazarListesi;
+            return View(p);
+
+        }
+        [HttpPost]
+        public ActionResult Guncelle(TBL_KITAP p)
+        {
+            var temp = db.TBL_KITAP.Find(p.ID);
+            temp.AD = p.AD;
+            temp.KATEGORI = p.KATEGORI;
+            temp.YAZAR = p.YAZAR;
+            temp.BASIMYIL = p.BASIMYIL;
+            temp.YAYINEVI = p.YAYINEVI;
+            temp.SAYFA = p.SAYFA;
+            temp.DURUM = p.DURUM;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
